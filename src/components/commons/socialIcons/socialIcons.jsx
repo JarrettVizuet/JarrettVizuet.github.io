89f6223fd9f2942c6df50c1d93c675bb34834f1e
Jarrett Vizuet <77868043+JarrettVizuet@users.noreply.github.com>
2026-03-24 22:58:23 -0600

    fix url linkedin

--- src/components/commons/socialIcons/socialIcons.jsx
+++ src/components/commons/socialIcons/socialIcons.jsx
@@ -9,7 +9,7 @@ const SocialIcons = () => {
   return (
     <div className="my-5 flex items-center justify-center">
       <a
-        href="www.linkedin.com/in/jarrettvizuet/"
+        href="https://www.linkedin.com/in/jarrettvizuet/"
         target="_blank"
         rel="noopener noreferrer"
         className="mr-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
